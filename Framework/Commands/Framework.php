812f3702b958350a621abe30fa7c383fcec9368a
Michael Yoo <sekjun9878@gmail.com>
2013-08-23 23:26:16 +0930

    Added Plugin-based directory structure.

--- Framework/Commands/Framework.php
+++
@@ -1,3 +0,0 @@
-<?
-
-$this->api->chat->broadcast("This server is using Framework v1.0.0");
--- Framework/Libs/BootStrap.php
+++ Framework/Libs/BootStrap.php
@@ -17,15 +17,16 @@ class BootStrap
 	public function __construct($data)
 	{
 		$this->Registered = new stdClass();
+		$this->Registered->Plugins = new stdClass();
 		$this->Registered->Commands = new stdClass();
 
 		$this->path = $data['path'];
 		$this->api = $data['api'];
 		$this->server = $data['server'];
 
-		$this->config['config'] = new Config($this->path . "Commands.yml", CONFIG_YAML, array(
-			"Commands" => array(
-				"framework" => ""
+		$this->config['config'] = new Config($this->path . "Plugins.yml", CONFIG_YAML, array(
+			"Plugins" => array(
+				"Demo"
 			),
 		));
 
@@ -35,9 +36,14 @@ class BootStrap
 	public function mapCommands()
 	{
 		$this->config['config']->reload();
-		foreach ($this->config['config']->get("Commands") as $key => $value) {
-			$this->Registered->Commands->$key = new stdClass();
-			$this->Registered->Commands->$key = $value;
+		foreach ($this->config['config']->get("Plugins") as $key) {
+			$this->Registered->Plugins->$key = new stdClass();
+			$this->Registered->Plugins->$key = true;
+			$config = new Config($this->path . "/Plugins/" . ucfirst(strtolower($key)) . "/" . "Commands.yml", CONFIG_YAML, false);
+			foreach ($config->getAll(true) as $keyd) {
+				$temp = strtolower($keyd);
+				$this->Registered->Commands->$temp = ucfirst(strtolower($key));
+			}
 		}
 	}
 
@@ -50,45 +56,53 @@ class BootStrap
 		}
 		foreach ($this->Registered->Commands as $key => $value) {
 			if (strtolower($key) == $data['cmd']) {
-				if (file_exists($this->path . "Commands/" . ucfirst(strtolower($key)) . ".php") == true) {
-					$Container = new Container(
+				if (file_exists($this->path . "Plugins/$value/Commands/" . ucfirst(strtolower($key)) . ".php") == true) {
+					$FrameworkContainer = new FrameworkContainer(
 						array(
-							"path" => $this->path . "Commands/" . ucfirst(strtolower($key)) . ".php",
-							"api" => $this->api,
-							"server" => $this->server,
+							"Path" => $this->path . "Plugins/$value/Commands/" . ucfirst(strtolower($key)) . ".php",
+							"API" => $this->api,
+							"Server" => $this->server,
 						),
 						$data
 					);
+					unset($FrameworkContainer);
 				} else {
 					console("Warning: Failed to perform command $key. Failed to open file.");
 				}
 			} else {
-				console("Command doesn't exist! Use /help\n");
+				console("Command doesn't exist! Use /help");
 			}
 		}
 	}
 }
 
-class Container
+class FrameworkContainer
 {
-	private $API, $Server, $Player, $Level;
+	private $API, $Server, $Player, $Level, $Env;
 
-	public function _construct($env, $data)
+	public function __construct($env, $data)
 	{
-		$this->API = $env['api'];
-		$this->Server = $env['server'];
+		$this->API = $env['API'];
+		$this->Server = $env['Server'];
 		if (isset($data['issuer'])) {
 			$this->Player = $data['issuer'];
 		} else if (isset($data['player'])) {
 			$this->Player = $data['player'];
 		}
 
-		$this->Level = $this->Player->level;
+		if (isset($this->Player->level)) {
+			$this->Level = $this->Player->level;
+		}
 
-		include($env['path']);
+		$this->Env = new stdClass();
+		foreach ($env as $key => $value) {
+			$this->Env->$key = $value;
+		}
+
+		include($this->Env->Path);
 	}
 
-	public function _destruct()
+	public function __destruct()
 	{
 
 	}
